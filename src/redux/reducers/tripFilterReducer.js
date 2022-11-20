1f4a36b1e7a47e62a5912a853272956b49217565
Alberto <amarturelo@gmail.com>
2022-11-20 02:32:06 -0300

    filter logic

--- src/redux/reducers/tripFilterReducer.js
+++ src/redux/reducers/tripFilterReducer.js
@@ -1,11 +1,36 @@
 const inititalState = {filter: null, trip: null};
 const tripReviewReducer = (state = inititalState, action) => {
-  if (action.trips != null) {
-    switch (action.type) {
-      case 'SELECT_TRIP_FILTER': {
-        const trip = Object.entries(action.trips).values();
-        return {...state, filter: action.filter, };
-      }
+  switch (action.type) {
+    case 'SELECT_TRIP_FILTER': {
+      //TODO Add filter logic
+      return {
+        ...state,
+        filter: action.filter,
+        trip: {
+          date: '2022-02-01',
+          averageSpeedInMph: 55,
+          tripDetails: [
+            {
+              id: 'TRTO',
+              name: 'Toronto',
+              arrivedTime: null,
+              departedTime: '10:55',
+            },
+            {
+              id: 'GUIL',
+              name: 'Guildwood',
+              arrivedTime: '11:30',
+              departedTime: '11:55',
+            },
+            {
+              id: 'OSHA',
+              name: 'Oshawa',
+              arrivedTime: '12:30',
+              departedTime: null,
+            },
+          ],
+        },
+      };
     }
   }
   return state;
